50e652ea4e3fb2ded400b53d7bc91673efee30b8
Dkreick <damian.kreick@gmail.com>
2018-05-21 18:21:07 -0300

    Align canvas and load background

--- src/Gameplay.js
+++ src/Gameplay.js
@@ -1,20 +1,22 @@
 GamePlayManager = {
 
     init: function () {
-        console.log("init");
+        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        game.scale.pageAlignHorizontally = true;
+        game.scale.pageAlignVertically = true;
     },
     preload: function () {
-        console.log("preload");
+        game.load.image("background", "assets/images/background.png");
     },
     create: function () {
-        console.log("create");
+        game.add.sprite(0, 0, "background");
     },
     update: function () {
         console.log("update");
     }
 }
 
-var game = new Phaser.Game(1136, 640, Phaser.AUTO);
+var game = new Phaser.Game(1136, 640, Phaser.CANVAS);
 
 game.state.add("gameplay", GamePlayManager);
 game.state.start("gameplay");
